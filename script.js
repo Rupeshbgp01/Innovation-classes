057238df20252264f0e80a0ac91c1a7bbd78e71a
Rupesh Kumar <rkchourasia2001@gmail.com>
2022-10-05 07:41:58 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let navbar=document.querySelector(".navbar");
+let navlist=document.querySelector(".nav-list");
+let icon=document.querySelector(".icon");
+icon.onclick=function(){
+    navbar.classList.toggle("new-nav-resp");
+    navlist.classList.toggle("vis-resp")
+}
+
+
